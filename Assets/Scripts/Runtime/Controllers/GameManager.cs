a6110cd9a619cc4dc8c0b28b47fb2fb158c2f533
Alihan AÇIKGÖZ <alihanacikgoz@yahoo.com>
2025-02-07 17:08:16 +0300

    Game Manager debugged.

--- Assets/Scripts/Runtime/Controllers/GameManager.cs
+++ Assets/Scripts/Runtime/Controllers/GameManager.cs
@@ -1,171 +1,70 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using NaughtyAttributes;
-using UnityEngine;
-using UnityEngine.Serialization;
-
-namespace Runtime.Controllers
-{
-    public class GameManager : MonoBehaviour
-    {
-        [Foldout("Touches"), SerializeField] private Touch[] _touches;
-        [Foldout("Touches"), SerializeField] private GameObject[] vfxPrefabs, fingers;
-
-        [Foldout("Countdown"), SerializeField] private GameObject[] countdownVfx;
-        [Foldout("Countdown"), SerializeField] private float countdownTime, currentCountdownTime;
-        [Foldout("Countdown"), SerializeField] private bool countdownStarted;
-
-        [Foldout("Selection"), SerializeField] private bool selecting = false;
-        [Foldout("Selection"), SerializeField] private float selectionTime, speedDecreaseFactor, currentSpeed, scaleFactor;
-        [Foldout("Selection"), SerializeField] private int selectionCycles;
-
-        [Foldout("Dictionary")] public Dictionary<int, GameObject> FingerDictionary = new Dictionary<int, GameObject>();
-
-        private void Update()
-        {
-            int touchCount = Input.touchCount;
-
-            if (touchCount>0)
-            {
-
-                _touches = new Touch[touchCount];
-
-                for (int i = 0; i < touchCount; i++)
-                {
-                    _touches[i] = Input.GetTouch(i);
-                    Touch touch = _touches[i];
-                    int fingerId = touch.fingerId;
-
-                    switch (_touches[i].phase)
-                    {
-                        case TouchPhase.Began:
-
-                            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(_touches[i].position);
-                            GameObject newVfx = Instantiate(vfxPrefabs[Random.Range(0, vfxPrefabs.Length)], touchPosition, Quaternion.Euler(90, 0, 0));
-                            ParticleSystem[] childerenParticles = newVfx.GetComponentsInChildren<ParticleSystem>();
-                            foreach (ParticleSystem particle in childerenParticles)
-                            {
-                                var main = particle.main;
-                                main.startColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-                            }
-
-                            if (!FingerDictionary.ContainsKey(fingerId))
-                            {
-                                FingerDictionary.Add(fingerId, newVfx);
-                            }
-                            break;
-                        case TouchPhase.Ended:
-                            if (FingerDictionary.TryGetValue(fingerId, out GameObject obj))
-                            {
-                                Destroy(obj);
-                                FingerDictionary.Remove(fingerId);
-                            }
-                            break;
-                        case TouchPhase.Stationary:
-
-                            break;
-                        case TouchPhase.Canceled:
-                            if (FingerDictionary.TryGetValue(fingerId, out GameObject obj2))
-                            {
-                                Destroy(obj2);
-                                FingerDictionary.Remove(fingerId);
-                            }
-                            break;
-                    }
-                }
-            }
-            else
-            {
-                if (Input.GetMouseButtonDown(0))
-                foreach (GameObject finger in fingers)
-                {
-                    Destroy(finger);
-                }
-                FingerDictionary.Clear();
-            }
-        }
-
-        private IEnumerator StartCountdown()
-        {
-
-
-            yield return new WaitForSeconds(.5f);
-            //StartCoroutine(RandomSelection());
-        }
-
-        private IEnumerator RandomSelection()
-        {
-            selecting = true;
-            int index = 0;
-
-            float elapsedTime = 0f;
-            float cycleDuration = selectionTime / selectionCycles;
-
-            while (selecting)
-            {
-                if (vfxPrefabs != null)
-                {
-                    index = Random.Range(0, vfxPrefabs.Length);
-
-                    foreach (GameObject vfx in vfxPrefabs)
-                    {
-                        vfx.transform.localScale = Vector3.one;
-                    }
-
-                    if (vfxPrefabs[index] != null)
-                    {
-
-                    }
-
-                }
-
-            }
-            yield return null;
-        }
-    }
-}*/
-
-
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using NaughtyAttributes;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 namespace Runtime.Controllers
 {
     public class GameManager : MonoBehaviour
     {
-        
+        #region Main
 
-        [Foldout("Main Music"), SerializeField] private AudioClip winSound;
-        
+        [Foldout("Main Music"), SerializeField]
+        private AudioClip winSound;
+
+        #endregion
+
+        #region Touches
+
         [Foldout("Touches"), SerializeField] private Touch[] _touches;
         [Foldout("Touches"), SerializeField] private GameObject[] vfxPrefabs, fingers;
+
+        #endregion
+
+        #region Countdown
 
         [Foldout("Countdown"), SerializeField] private GameObject[] countdownVfx;
         [Foldout("Countdown"), SerializeField] private float countdownTime = 3f;
         [Foldout("Countdown"), SerializeField] private bool countdownStarted;
-        private Coroutine _countdownCoroutine; // Coroutine referansı tut
+
+        #endregion
+
+        #region Selection
 
         [Foldout("Selection"), SerializeField] private bool selecting = false;
         [Foldout("Selection"), SerializeField] private AudioClip selectionMusic;
         [Foldout("Selection"), SerializeField] private AudioClip highlightSound;
-        [Foldout("Selection"), SerializeField] private float selectionTime = 5f, speedDecreaseFactor = 0.9f, currentSpeed = 1f, scaleFactor = 1.1f;
-        [Foldout("Selection"), SerializeField] private int selectionCycles = 5;
-        
-        [Foldout("Explosion"), SerializeField] private GameObject explosionPrefab;
-        [Foldout("Explosion"), SerializeField] private AudioClip explosionSound; 
 
-        
-        
-        
+        [Foldout("Selection"), SerializeField]
+        private float selectionTime = 5f, speedDecreaseFactor = 0.9f, currentSpeed = 1f, scaleFactor = 1.1f;
+
+        [Foldout("Selection"), SerializeField] private int selectionCycles = 5;
+
+        #endregion
+
+        #region Explosion
+
+        [Foldout("Explosion"), SerializeField] private GameObject explosionPrefab;
+        [Foldout("Explosion"), SerializeField] private AudioClip explosionSound;
+
+        #endregion
+
+        #region Dictionary
 
         [Foldout("Dictionary")] public Dictionary<int, GameObject> FingerDictionary = new Dictionary<int, GameObject>();
+
+        #endregion
+
+        #region Private
+
+        private Coroutine _countdownCoroutine; // Coroutine referansı tut
         private List<GameObject> instantiatedVfxObjects = new List<GameObject>();
         private AudioSource _audioSource;
+
+        #endregion
+
+
         private void Start()
         {
             _audioSource = gameObject.GetComponent<AudioSource>();
@@ -321,7 +220,7 @@ namespace Runtime.Controllers
 
             yield return new WaitForSeconds(0.5f);
             _audioSource.PlayOneShot(winSound);
-            
+
             yield return new WaitForSeconds(1f); // 3 saniye bekle
 
             // 🎆 Patlama efekti ve yok etme
@@ -331,12 +230,12 @@ namespace Runtime.Controllers
             // 🎵 Müziği durdur
             _audioSource.Stop();
         }
-        
+
         private void InstantiateExplosion(Vector3 position)
         {
             if (explosionPrefab != null)
             {
-                Instantiate(explosionPrefab, position, Quaternion.Euler(90,0,0));
+                Instantiate(explosionPrefab, position, Quaternion.Euler(90, 0, 0));
 
                 // 🎵 Patlama sesi çal
                 if (explosionSound != null)
@@ -345,7 +244,7 @@ namespace Runtime.Controllers
                 }
             }
         }
-        
+
         private void ResetCountdown()
         {
             if (countdownStarted)
